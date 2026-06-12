b35848958271890e1305e9ea1509e80a438ff55f
Mario Kernich <mario.kernich@swan.de>
2026-06-12 20:27:50 +0200

    Initial Commit

---
+++ blog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  blog
+//
+//  Created by Mario on 12.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ blog/blogApp.swift
@@ -0,0 +1,17 @@
+//
+//  blogApp.swift
+//  blog
+//
+//  Created by Mario on 12.06.26.
+//
+
+import SwiftUI
+
+@main
+struct blogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
